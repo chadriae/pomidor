cf1a4f087a6cf29e341c7e7ccc72b9744884b0b1
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-21 11:33:01 +0200

    Setup react automatic runtime

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
-
 module.exports = {
     extends: "@becode",
+    rules: [
+        "react/jsx-uses-react", 0,
+        "react/react-in-jsx-scope", 0,
+    ],
 };
--- src/components/hello.js
+++ src/components/hello.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Hello = ({name}) => <p>{`Hello, ${name}!`}</p>;
 
 export default Hello;
