f0560f5ef9306f08e13d7450191ac6e7baf9d8bf
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-21 15:49:40 +0200

    Handle rest props in button

--- src/components/tools/button.js
+++ src/components/tools/button.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
-const Button = ({label, title, disabled=false, onClick}) => (
+const Button = ({label, title, disabled=false, onClick, ...props}) => (
     <button
         className={classnames("button", "is-link", "is-medium", "is-fullwidth")}
         type={"button"}
         label={title|label}
         disabled={disabled}
         onClick={onClick}
+        {...props}
     >
         {label}
     </button>
